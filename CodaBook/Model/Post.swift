75b4d847eacf4e39c9fc0359f38699e9164d710e
Snoopy <snoopy@MacBook-Pro-de-SERGENT.local>
2018-08-18 15:19:56 +0200

    ajout des likes

--- CodaBook/Model/Post.swift
+++ CodaBook/Model/Post.swift
@@ -14,6 +14,7 @@ class Post {
     private var _utilisateur : Utilisateur
     private var _texte : String
     private var _imageUrl : String?
+    private var _likes: [String]
     
     var id: String {
         return _id
@@ -21,6 +22,10 @@ class Post {
     
     var date: Double {
         return _date
+    }
+    
+    var likes: [String] {
+        return _likes
     }
     
     var utilisateur: Utilisateur {
@@ -41,6 +46,11 @@ class Post {
         self._utilisateur = utilisateur
         self._texte = texte
         self._imageUrl = imageUrl
+        self._likes = []
+    }
+    
+    func maj(likes: [String]) {
+        self._likes = likes
     }
     
     

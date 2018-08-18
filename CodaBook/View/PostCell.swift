6e5a6df69fa1b518bda328748e045a176e1356e6
Snoopy <snoopy@MacBook-Pro-de-SERGENT.local>
2018-08-18 14:19:43 +0200

    design PostCell

---
+++ CodaBook/View/PostCell.swift
@@ -0,0 +1,41 @@
+//
+//  PostCell.swift
+//  CodaBook
+//
+//  Created by Snoopy on 18/08/2018.
+//  Copyright © 2018 EMANET. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+    @IBOutlet weak var photoDeProfil: ImageRonde!
+    @IBOutlet weak var nomEtPrenom: UILabel!
+    
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var imageDuPost: UIImageView!
+    @IBOutlet weak var nombreDeLike: UILabel!
+    
+    @IBOutlet weak var boutonLike: UIButton!
+    @IBOutlet weak var texteDuPost: UILabel!
+    
+    
+    
+    var post: Post!
+    
+    func miseEnPlace(post: Post) {
+        self.post = post
+        photoDeProfil.telecharger(self.post.utilisateur.imageUrl)
+        nomEtPrenom.text = self.post.utilisateur.prenom + "  "+ self.post.utilisateur.nom
+        imageDuPost.telecharger(self.post.imageUrl)
+        texteDuPost.text = self.post.texte
+        
+    }
+    
+    
+    
+    @IBAction func likeAppuye(_ sender: Any) {
+    }
+    
+}
